6aeb53e5e19c006175fa87c30cbb151a43654093
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-07-12 14:46:52 +0900

    [Del] UIFont+ 삭제
    - 임시 제거

--- Sources/HeeKit/UIFont/UIFont+.swift
+++
@@ -1,7 +0,0 @@
-import UIKit.UIFont
-
-extension UIFont {
-    public static func font(_ type: TaskPriority, ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: type.rawValue, size: size)!
-    }
-}
