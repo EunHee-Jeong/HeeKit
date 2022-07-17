2e6f7987a04c635cdbf42aacbdc9080eb55181e0
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-07-17 17:25:03 +0900

    [Edit] 네이밍 수정
    - 알아보기 쉽도록 하기 위해 효과를 적용할 버튼으로 수정

--- Sources/HeeKit/Extension/UIButton/UIButton+.swift
+++ Sources/HeeKit/Extension/UIButton/UIButton+.swift
@@ -1,15 +1,15 @@
 import UIKit.UIButton
 
 extension UIButton {
-    public func setUnderline(at targetString: UIButton, state: UIControl.State) {
-        let title = targetString.title(for: state)
+    public func setUnderline(at targetButton: UIButton, state: UIControl.State) {
+        let title = targetButton.title(for: state)
         let attributedString = NSMutableAttributedString(string: title ?? "")
         attributedString.addAttribute(
             .underlineStyle,
             value: NSUnderlineStyle.single.rawValue,
             range: NSRange(location: 0, length: title?.count ?? 0)
         )
-        targetString.setAttributedTitle(attributedString, for: state)
+        targetButton.setAttributedTitle(attributedString, for: state)
     }
     
 }
