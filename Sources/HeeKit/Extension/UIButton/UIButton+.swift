e8e0f0ead660eb8b806d0fd4f7ab1882ad3e70e6
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-07-17 17:03:18 +0900

    [Fix] 오류 수정

--- Sources/HeeKit/Extension/UIButton/UIButton+.swift
+++ Sources/HeeKit/Extension/UIButton/UIButton+.swift
@@ -1,6 +1,6 @@
 import UIKit.UIButton
 
-public func setUnderline(at targetString: UIButton, state: UIControl.State) -> UIButton {
+public func setUnderline(at targetString: UIButton, state: UIControl.State) {
     let title = targetString.title(for: state)
     let attributedString = NSMutableAttributedString(string: title ?? "")
     attributedString.addAttribute(
@@ -8,5 +8,5 @@ public func setUnderline(at targetString: UIButton, state: UIControl.State) -> U
         value: NSUnderlineStyle.single.rawValue,
         range: NSRange(location: 0, length: title?.count ?? 0)
     )
-    return targetString.setAttributedTitle(attributedString, for: state)
+    targetString.setAttributedTitle(attributedString, for: state)
 }
