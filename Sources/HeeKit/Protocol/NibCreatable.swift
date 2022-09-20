4b55a757ba2e78c210daed13ab05be29caa351d5
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-09-20 16:50:15 +0900

    [Del] NibCreatable

--- Sources/HeeKit/Protocol/NibCreatable.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-public protocol NibCreatable {
-    static var nib: UINib { get }
-    static var nibBundle: Bundle? { get }
-    static var nibName: String { get }
-    static func instanceFromNib() -> Self
-}
