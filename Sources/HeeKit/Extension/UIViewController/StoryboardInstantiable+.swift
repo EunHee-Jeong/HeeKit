9ba57c4310aea96384862aef613dad6fd0985f5d
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-09-20 16:49:45 +0900

    [Add] StoryboardInstantiable

---
+++ Sources/HeeKit/Extension/UIViewController/StoryboardInstantiable+.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+extension StoryboardInstantiable where Self: UIViewController {
+    static var defaultFileName: String {
+        return NSStringFromClass(Self.self).components(separatedBy: ".").last!
+    }
+    
+    static func instantiateViewController(_ bundle: Bundle? = nil) -> Self {
+        let fileName = defaultFileName
+        let storyboard = UIStoryboard(name: fileName, bundle: bundle)
+        guard let vc = storyboard.instantiateInitialViewController() as? Self
+        else {
+            fatalError("Cannot instantiate initial viewController \(Self.self) from storyboard with name \(fileName)")
+        }
+        return vc
+    }
+}
---
+++ Sources/HeeKit/Protocol/StoryboardInstantiable.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+public protocol StoryboardInstantiable: NSObjectProtocol {
+    associatedtype T
+    static var defaultFileName: String { get }
+    static func instantiateViewController(_ bundle: Bundle?) -> T
+}
